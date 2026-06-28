80c9a255797b8d3ab22ec0c743cb007c8075643d
davidnolen <david.nolen@gmail.com>
2026-06-28 12:48:33 -0400

    make sure that top levels belong to the correct namespace

--- plugins/processClosureNamespaces.js
+++ plugins/processClosureNamespaces.js
@@ -1,6 +1,18 @@
 const path = require("path");
+const fileToNamespace = {};
 const moduleRegex = /goog\.module\(\'([\w.]+)\'\)/;
 const provideRegex = /goog\.provide\(\'([\w.]+)\'\)/;
+
+function isTopLevel(doclet) {
+    if(!doclet.name) return false;
+    switch(doclet.kind) {
+    case "function":
+    case "class":
+        return true;
+    default:
+        return false;
+    }
+}
 
 exports.handlers = {
     beforeParse(e) {
@@ -9,6 +21,16 @@ exports.handlers = {
         if(match = moduleRegex.exec(e.source) || provideRegex.exec(e.source)) {
             let ns = match[1];
             e.source += "\n/** @namespace " + ns + " */\n"
+            fileToNamespace[path.basename(e.filename)] = ns;
+        }
+    },
+
+    newDoclet(e) {
+        let doclet = e.doclet,
+            ns     = fileToNamespace[doclet.meta.filename];
+
+        if(isTopLevel(doclet)) {
+            doclet.memberof = ns;
         }
     }
 }
